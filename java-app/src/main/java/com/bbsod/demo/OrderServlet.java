ba54ffa75315e61f3d4931f8df3309f811e494bf
zahcen <zahcen@gmail.com>
2025-09-08 14:30:40 +0200

    CI/CD

--- java-app/src/main/java/com/bbsod/demo/OrderServlet.java
+++ java-app/src/main/java/com/bbsod/demo/OrderServlet.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,6 +18,8 @@ import java.util.Random;
 
 import org.json.JSONObject;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.extension.annotations.SpanAttribute;
 import io.opentelemetry.extension.annotations.WithSpan;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -24,7 +28,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class OrderServlet extends HttpServlet {
 
-    @WithSpan("getDBdata")
+    String payment_method="";
+    boolean payment_status = false;
+
+    @WithSpan()
     protected void getDBdata(){
         // JDBC connection parameters
         String jdbcUrl = "jdbc:mysql://ht-mysql:3306/mydatabase?useSSL=false&allowPublicKeyRetrieval=true";
@@ -64,7 +71,7 @@ public class OrderServlet extends HttpServlet {
     }
 
     @WithSpan("Validate Shipping Address")
-    private void validate_shipping_address(boolean addMetrics){
+    private void validate_shipping_address(){
 
         try {
             Thread.sleep(600);
@@ -74,7 +81,7 @@ public class OrderServlet extends HttpServlet {
     }
 
     @WithSpan()
-    private void validate_billing_address(boolean addMetrics){
+    private void validate_billing_address(){
         // Sleep for 2 seconds
         try {
             Thread.sleep(400);
@@ -84,74 +91,7 @@ public class OrderServlet extends HttpServlet {
     }
 
     @WithSpan()
-    private void validate_payment(boolean payment_status, boolean addMetrics){
-        try {
-
-            Thread.sleep(300);
-
-            if (! payment_status) {
-                throw new RuntimeException("Payment failed: Card declined");
-            }
-
-        } catch (Exception e) {
-            // Attach exception details to the span
-        } 
-    }
-
-        @WithSpan()
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-
-        
-        String addMetricsParam  = request.getParameter("add_metrics"); // "true" or "false"
-        boolean addMetrics = Boolean.parseBoolean(addMetricsParam);
-        System.out.println("addMetrics="+addMetrics);
-        
-        //Span span = null;
-        //if (addMetrics)
-        //    span = tracer.spanBuilder("PlaceOrder").startSpan();
-        
-        String payment_method="";
-        boolean payment_status=true;
-        
-        Random random = new Random();
-
-        // Generate random orderId between 1000 and 2000 (inclusive)
-        int orderId_int = 1000 + random.nextInt(2000 - 1000 + 1);
-        String orderId = Integer.toString(orderId_int);
-
-        // Generate random customerId between 10000 and 20000 (inclusive)
-        int customerId_int = 10000 + random.nextInt(20000 - 10000 + 1);
-        String customerId = Integer.toString(customerId_int);
-
-        System.out.println("Generated Order ID: " + orderId);
-        System.out.println("Generated Customer ID: " + customerId);
-
-        getDBdata();
-
-        validate_shipping_address(addMetrics);
-
-        // Add useful attributes to the span
-        double orderAmount = 10 + Math.random() * 500;
-        String amount = String.format("%.2f", orderAmount);
-
-        try {
-            //if (addMetrics)
-            //    span.setAttribute("order_amount", orderAmount);
-            if (orderAmount<100){
-            //    if (addMetrics)
-            //        span.setAttribute("payment_method", "Invoice");
-                payment_method="Invoice";
-            }
-            else{
-            //    if (addMetrics)
-            //        span.setAttribute("payment_method", "Credit card");
-                payment_method="Credit card";
-            }
-
-        getDBdata();
-        validate_billing_address(addMetrics);
-
+    private void validate_payment(String orderId, String customerId, double orderAmount) throws MalformedURLException, ProtocolException, IOException{
 
         // Get Node.js URL from environment variable, default to http://localhost:3000/order
         String nodeJsUrl = System.getenv("NODEJS_ORDER_URL");
@@ -198,49 +138,74 @@ public class OrderServlet extends HttpServlet {
         // Parse the JSON
         JSONObject jsonObject = new JSONObject(json);
 
-        // Extract values
-        orderId = jsonObject.getString("order_id");
-        customerId = jsonObject.getString("customer_id");
         payment_status = "1".equals(jsonObject.getString("payment_status"));
-
-        //if (addMetrics)
-        //    span.setAttribute("order.id", orderId);
-
-        validate_payment(payment_status, addMetrics);
-/*
-        if (! payment_status) {
+        if (! payment_status)
             throw new RuntimeException("Payment failed: Card declined");
+
+    }
+
+    @WithSpan()
+    protected void placeOrder(@SpanAttribute("order_id") String orderId, String customerId, @SpanAttribute("order_amount") double order_amount) throws ProtocolException, IOException{
+        
+        
+        validate_shipping_address();
+        
+        if (order_amount<100){
+            payment_method="Invoice";
         }
-        } catch (Exception e) {
-            if (addMetrics){
-                span.recordException(e);
-                span.setStatus(StatusCode.ERROR, "Payment failed");
-            }
-*/
-
-        } finally {
-            // Always end the span
-            //if (addMetrics)
-            //    span.end();
-            
-            // Set response type to JSON
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-
-            // Build JSON response
-            JSONObject jsonResponse = new JSONObject();
-            jsonResponse.put("order_id", orderId);
-            jsonResponse.put("customer_id", customerId);
-            jsonResponse.put("amount", amount);
-            jsonResponse.put("payment_method", payment_method);
-            jsonResponse.put("payment_status", payment_status);
-
-
-            // Send JSON to client
-            PrintWriter out = response.getWriter();
-            out.print(jsonResponse.toString());
-            out.flush();
+        else{
+            payment_method="Credit card";
         }
+
+        Span currentSpan = Span.current();
+        currentSpan.setAttribute("payment_method", payment_method);
+
+        getDBdata();
+
+        validate_billing_address();
+        validate_payment(orderId, customerId, order_amount);
+
+
+    }
+
+    @WithSpan()
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+        
+        Random random = new Random();
+        // Generate random orderId between 1000 and 2000 (inclusive)
+        int orderId_int = 1000 + random.nextInt(2000 - 1000 + 1);
+        String orderId = Integer.toString(orderId_int);
+
+        // Generate random customerId between 10000 and 20000 (inclusive)
+        int customerId_int = 10000 + random.nextInt(20000 - 10000 + 1);
+        String customerId = Integer.toString(customerId_int);
+
+        System.out.println("Generated Order ID: " + orderId);
+        System.out.println("Generated Customer ID: " + customerId);
+
+        getDBdata();
+        double orderAmount = 10 + Math.random() * 500;
+        String amount = String.format("%.2f", orderAmount);
+
+        placeOrder(orderId, customerId, orderAmount);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // Build JSON response
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("order_id", orderId);
+        jsonResponse.put("customer_id", customerId);
+        jsonResponse.put("amount", amount);
+        jsonResponse.put("payment_method", payment_method);
+        jsonResponse.put("payment_status", payment_status);
+
+
+        // Send JSON to client
+        PrintWriter out = response.getWriter();
+        out.print(jsonResponse.toString());
+        out.flush();
     }
 
 }
