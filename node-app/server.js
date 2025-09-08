f86c57efda9481d58940f3c91b3da0f8c2919965
zahcen <zahcen@gmail.com>
2025-09-08 16:37:50 +0200

    CI/CD

--- node-app/server.js
+++ node-app/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { trace } = require('@opentelemetry/api');
+//const { trace } = require('@opentelemetry/api');
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Allow dynamic port for Kubernetes
